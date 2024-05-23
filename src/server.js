550127e87b57952a74c16273d9d2924fbcb3388d
shtjrgus010 <shtjrgus010@naver.com>
2024-05-24 00:42:20 +0900

    server, front setting

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { fileURLToPath } from "url"; 
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url)); 
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+const handleListen = () => console.log('Listening on http://localhost:3000');
+
+app.listen(3000, handleListen);
