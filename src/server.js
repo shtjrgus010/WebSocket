81aadcbfbc8b1169275aaae116a9c34f6b528df7
shtjrgus010 <shtjrgus010@naver.com>
2024-05-25 23:06:05 +0900

    WebSocket in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket, {WebSocketServer} from "ws";
 import { fileURLToPath } from "url"; 
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url)); 
@@ -7,7 +9,17 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-const handleListen = () => console.log('Listening on http://localhost:3000');
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.listen(3000, handleListen);
+const handleListen = () => console.log('Listening on :http://localhost:3000');
+
+//http 서버 생성
+const server = http.createServer(app);
+
+//websocket 서버 생성
+const wss = new WebSocketServer({server});
+// http와 websockt 서버는 동시에 생성 가능
+
+server.listen(3000, handleListen);
+
